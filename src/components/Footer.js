91b2c05207e6b97d9e4cf8cc563a8f117afcb046
Sirm-O <jabuyahsam@gmail.com>
2025-07-10 14:04:13 +0300

    new edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FacebookIcon, TwitterIcon } from 'react-share';
 import { MailIcon, PhoneIcon, LocationMarkerIcon } from '@heroicons/react/outline';
 
 const Footer = () => {
@@ -56,10 +55,14 @@ const Footer = () => {
             <h3 className="text-xl font-semibold mb-4">Social Media</h3>
             <div className="flex gap-4">
               <a href="https://facebook.com/utugischool" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">
-                <FacebookIcon size={32} round={true} />
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
+                </svg>
               </a>
               <a href="https://twitter.com/utugischool" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">
-                <TwitterIcon size={32} round={true} />
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+                </svg>
               </a>
               <a href="https://instagram.com/utugischool" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">
                 <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
